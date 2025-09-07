0a4f05dfe90d4588dd8cd23399fe40db02a0971c
DuckyQuackQuack01 <sergsfarc@gmail.com>
2025-09-07 22:32:27 +1000

    Player movement but no animation

--- Assets/Scripts/DuckMan/Player.cs
+++ Assets/Scripts/DuckMan/Player.cs
@@ -9,16 +9,27 @@ public class Player : MonoBehaviour
 
     bool isMoving = false;
 
+    public float moveSpeed = 2f;
+    private int currentTargetIndex = 0;
+    private Vector2[] loopPoints = new Vector2[] {
+        new Vector2(-3.207f, 6.749f),
+        new Vector2(-3.207f, 5.177f),
+        new Vector2(-5.214f, 5.177f), 
+        new Vector2(-5.214f, 6.749f)
+    };
+
 
     private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        rb = GetComponent<Rigidbody2D>();
     }
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MoveLoop());
+
     }
 
     // Update is called once per frame
@@ -26,15 +37,32 @@ public class Player : MonoBehaviour
     {
         if (isMoving)
         {
-            isMoving = true;
             audioManager.PlaySFX(audioManager.duckWalk, true);
-            Debug.Log("Should be playing");
+
         } else
         {
-            isMoving = false;
             audioManager.StopSFX();
+        }   
+    }
+
+    private IEnumerator MoveLoop()
+    {
+        while (true){
+            Vector2 target = loopPoints[currentTargetIndex];
+
+            while(Vector2.Distance(transform.position, target) > 0.05f)
+            {
+                isMoving = true;
+                transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            transform.position = target;
+
+            currentTargetIndex = (currentTargetIndex + 1) % loopPoints.Length;
+
+            isMoving = false;
         }
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
